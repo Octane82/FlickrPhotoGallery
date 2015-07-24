dcb02ce5b2ef3398ac5e55a657edcbe9b625169a
Octane_cadcam <octanek@hotmail.com>
2015-07-24 14:41:12 +0400

    Output photo in GridView from Flickr (ITS - 24.07.2015)

---
+++ app/src/main/java/com/example/octane/flickrphotogallery/GalleryItem.java
@@ -0,0 +1,7 @@
+package com.example.octane.flickrphotogallery;
+
+/**
+ * Created by ITS on 24.07.2015.
+ */
+public class GalleryItem {
+}
---
+++ app/src/main/java/com/example/octane/flickrphotogallery/net/ThumbnailDownloader.java
@@ -0,0 +1,7 @@
+package com.example.octane.flickrphotogallery.net;
+
+/**
+ * Created by ITS on 24.07.2015.
+ */
+public class ThumbnailDownloader {
+}
